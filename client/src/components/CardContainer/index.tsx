52165dea2244307c43aba753e854bc0b983ecd4c
Rubtsov Oleg <rubtsovoa@gmail.com>
2025-09-19 15:17:07 +0400

    Fix readme

--- client/src/components/CardContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { PropsWithChildren } from 'react';
-import { Styled } from './CardContainer.styles';
-
-function CardContainer({ children }: PropsWithChildren) {
-    return <Styled.Container>{children}</Styled.Container>;
-}
-
-export default CardContainer;
